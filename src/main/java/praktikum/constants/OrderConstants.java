267b1fc68598cbd332969bbbcfcc5a70aae95cb0
Aleksahdr Daniliants <isken2046@yandex.ru>
2024-10-23 23:16:51 +0300

    Add OrderConstants.java

---
+++ src/main/java/praktikum/constants/OrderConstants.java
@@ -0,0 +1,12 @@
+package praktikum.constants;
+
+public class OrderConstants {
+
+    public static final int INGREDIENTS_COUNT = 10;
+
+    public static final String GET_INGREDIENTS_LIST_PATH = "/api/ingredients";
+    public static final String CREATE_ORDER_PATH = "/api/orders";
+    public static final String GET_ORDERS_PATH = "/api/orders";
+
+    public static final String INGREDIENTS_NEEDED_MESSAGE = "Ingredient ids must be provided";
+}
